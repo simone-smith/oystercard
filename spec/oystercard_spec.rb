0d2d5311d62af26f33f43d0dc3f58380dcc81fd1
Simone <simonecaplin@gmail.com>
2018-05-21 15:20:09 +0100

    Challenge 05: adds top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,18 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  describe '#initialize' do
+    it 'initializes with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up the card with monies' do
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
+    end
+  end
+
 end
